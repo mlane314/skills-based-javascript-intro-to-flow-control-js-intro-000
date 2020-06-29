29eb0c98f04c7119d4dc4b2741dc43484fc5a087
Michael Lane <mlane314@gmail.com>
2020-06-29 00:45:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,13 @@
 function basicTeenager(age) {
   if (age>=13 && age<=19){
-  return "You are a teenager!";
-}
+  return "You are a teenager!"
+  }
 }
 
 function teenager(age){
   if (age >=13 && age <=19){
-  return "You are a Teenager!"
-  } else {return "You are not a teenager";}
+  return "You are a teenager!"
+} else if {return "You are not a teenager";}
 }
 
 function ageChecker(age){
